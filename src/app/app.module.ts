8384b30eacbf8a74a52a55f2fd2041c743a55333
mcwilliamska <mcwilliamska2016@gmail.com>
2021-03-26 13:04:11 -0400

    Revamp with nav and new structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,37 +4,35 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { HttpClientModule } from '@angular/common/http';
 
 import { HomeComponent } from './home/home.component';
+import { LayoutComponent } from './layout/layout.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 // Material Components
 
-import {LayoutModule} from '@angular/cdk/layout';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatSidenavModule} from '@angular/material/sidenav';
-import { NavigationComponent } from './navigation/navigation.component';
+import { MaterialModule } from './material/material.module';
+import { HeaderComponent } from './navigation/header/header.component';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavigationComponent,
+    LayoutComponent,
+    HeaderComponent,
+    SidenavListComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSidenavModule,
-    MatListModule,
+    FlexLayoutModule,
     HttpClientModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    LayoutModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,4 +12,8 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  public executeSelectedChange = (event) => {
+    console.log(event);
+  }
+
 }
