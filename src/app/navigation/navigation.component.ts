7ef5d5a7aeabc69e14492e8d7f688eff96c6aaa9
mcwilliamska <mcwilliamska2016@gmail.com>
2021-03-25 22:56:23 -0400

    Changed to ESLint from TSLint, installed HammerJS for gesture support

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { BreakpointObserver, BreakpointState, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-navigation',
@@ -8,36 +7,8 @@ import { BreakpointObserver, BreakpointState, Breakpoints } from '@angular/cdk/l
 })
 export class NavigationComponent implements OnInit {
 
+  constructor() { }
 
-
-  constructor(public breakpointObserver: BreakpointObserver) { }
-
-  ngOnInit(): void {
-    this.breakpointObserver.observe([
-      Breakpoints.XSmall,
-      Breakpoints.Small,
-      Breakpoints.Medium,
-      Breakpoints.Large,
-      Breakpoints.XLarge
-    ]).subscribe( (state: BreakpointState) => {
-      if (state.breakpoints[Breakpoints.XSmall]) {
-           console.log( 'Matches XSmall viewport');
-      }
-      if (state.breakpoints[Breakpoints.Small]) {
-           console.log( 'Matches Small viewport');
-      }
-      if (state.breakpoints[Breakpoints.Medium]) {
-           console.log( 'Matches Medium  viewport');
-      }
-      if (state.breakpoints[Breakpoints.Large]) {
-
-          console.log( 'Matches Large viewport');
-      }
-      if (state.breakpoints[Breakpoints.XLarge]) {
-
-         console.log( 'Matches XLarge viewport');
-      }
-    });
-  }
-
+  ngOnInit() { }
+    
 }
